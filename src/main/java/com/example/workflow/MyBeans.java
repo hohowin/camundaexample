dd793517a5a68e1f22afea24bcdb1ee3174b2632
Howin Ho <ho1ho@yahoo.com>
2022-04-21 17:34:15 -0400

    initial commit

---
+++ src/main/java/com/example/workflow/MyBeans.java
@@ -0,0 +1,14 @@
+package com.camundacon.workshop;
+
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBeans {
+
+    @Bean
+    public JavaDelegate ahoyService() {
+        return execution -> System.out.println("\n\nAhoy" + execution.getVariable("myVariable") + "!");
+    }
+}
